8d53698267e21e2d9faad95c6445d8ddff82d266
Vinh Le <lethanhvinh95@gmail.com>
2019-02-17 12:00:54 +0200

    Styling
    Add some styling

--- src/form.js
+++ src/form.js
@@ -2,25 +2,53 @@ import React, { useState } from "react";
 
 function Form() {
 	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
 
-	const submit = () => {
-		alert(firstName);
+	const submit = e => {
+		e.preventDefault();
+		console.log(firstName, lastName, email);
 	};
 
 	return (
-		<div>
-			<label htmlFor="fist-name">First name</label>
+		<form>
+			<label htmlFor="fist-name">
+				First name
+			</label>
 			<input
 				value={firstName}
 				type="text"
 				id="first-name"
-				onChange={e => setFirstName(e.target.value)}
+				onChange={e =>
+					setFirstName(e.target.value)
+				}
+				required
+			/>
+
+			<label htmlFor="last-name">Last name</label>
+			<input
+				value={lastName}
+				type="text"
+				id="last-name"
+				onChange={e =>
+					setLastName(e.target.value)
+				}
+				required
+			/>
+
+			<label htmlFor="email">Email</label>
+			<input
+				value={email}
+				type="email"
+				id="email"
+				onChange={e => setEmail(e.target.value)}
+				required
 			/>
 
 			<button type="submit" onClick={submit}>
 				Submit
 			</button>
-		</div>
+		</form>
 	);
 }
 
